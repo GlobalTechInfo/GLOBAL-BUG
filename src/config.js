e2288a59e455b76dfa283321e7276c326562c3b0
ditt210 <aditpastajal22@gmail.com>
2024-10-01 13:48:56 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 global.namaown = "Qasim Ali" // your owner name here
 global.namabot = "© GlobalTechInfo" // your bot name here
 global.versisc = "9.5.0" //
-global.owner = ["923444844060"] // your owner number here
+global.owner = ["6287848465638"] // your owner number here
 global.tele = "https://t.me/GlobalBotInc" // your telegram here
 global.url = "https://www.youtube.com/@GlobalTechInfo" // Your YouTube Here
 global.namastore = "LynnZxD" // UBAH NAMA STORE LU
